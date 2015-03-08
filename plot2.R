d6a69c6434be302d5b4824a45bde9ac7422e5619
Lars Plougmann <larsplougmann@PlougMac5K.local>
2015-03-08 00:53:42 -0600

    Done: Plot 1 and 2

---
+++ plot2.R
@@ -0,0 +1,37 @@
+plot2 <- function() {
+  
+  library(lubridate)
+  
+  ## The plot uses a dataset available at
+  ## https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip.
+  ## The code assumes that the data has been downloaded and extracted in a particular directory.
+  
+  ## Read data
+  orgdir <- getwd()
+  setwd("/Users/larsplougmann/Data/ExData_Plotting1")
+  
+  ## Read the entire data file
+  powerdata <- read.csv("household_power_consumption.txt", sep=";", 
+                        na.strings="?", 
+                        colClasses = c("character", "character", rep("numeric",7)))
+  setwd(orgdir)
+  
+  ## then pare it down to just the two days worth of data we are interested in
+  powerdata <- subset(powerdata, powerdata$Date == "1/2/2007" | powerdata$Date == "2/2/2007")
+  
+  ## convert date and time strings to a timestamp using lubridate
+  powerdata <- transform(powerdata, timestamp = dmy_hms(paste(powerdata$Date, powerdata$Time)))
+  
+  ## Now for the plotting.
+  
+  ## First specify that we are creating the plot in a PNG file
+  png(file = "plot2.png", width = 480, height = 480, units = "px")
+  ## Then go ahead and create the plot
+  
+  with(powerdata, plot(timestamp,Global_active_power, type="l",
+                       ylab = "Global Active Power (kilowatts)", xlab=""))
+  
+  ## And finish off by closing the device
+  dev.off()
+
+}
